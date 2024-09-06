8f15bf4046b523667113fae7a6f05bf728b8bfb8
nanachi <102416174+Swarkin@users.noreply.github.com>
2024-09-06 18:22:50 +0200

    Add bot field

--- addons/disdot/src/api/user.gd
+++ addons/disdot/src/api/user.gd
@@ -5,7 +5,9 @@ func _init(d: Dictionary) -> void:
 	id          = _try_take(d, "id"         , 0 ) as int
 	username    = _try_take(d, "username"   , "") as String
 	global_name = _try_take(d, "global_name", "") as String
+	bot         = _try_take(d, "bot"        , false) as bool
 
 var id: int
 var username: String
 var global_name: String
+var bot: bool
--- message_create.gd
+++ message_create.gd
@@ -2,3 +2,4 @@ extends MessageCreateEventHandler
 
 func _on_event(event: MessageCreateEvent) -> void:
 	print("MessageCreate received: ", event.message.content)
+	print("Bot? ", event.message.author.bot)
