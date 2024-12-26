db477eac578839eae25bbbfdd7467a69b92d7abd
swarkin <102416174+Swarkin@users.noreply.github.com>
2024-12-26 23:15:59 +0100

    Update reply.gd comment

--- addons/disdot/examples/commands/reply.gd
+++ addons/disdot/examples/commands/reply.gd
@@ -1,6 +1,6 @@
 extends CommandHandler
 
 func _on_command(ctx: CommandContext) -> void:
-	# reply with the raw message content if its from an user
-	if not ctx.message.author.bot:
-		await ctx.reply(ctx.message.content)
+	# reply with the raw message content
+	# bots are filtered by the 'Ignore Bots' property of CommandHandler, see inspector
+	await ctx.reply(ctx.message.content)
