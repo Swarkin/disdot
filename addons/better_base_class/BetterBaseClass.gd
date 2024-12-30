077da2500cee7df7a8c1023549c5c4f7f912e8d6
swarkin <102416174+Swarkin@users.noreply.github.com>
2024-12-30 15:11:47 +0100

    Update BetterBaseClass.gd

--- addons/better_base_class/BetterBaseClass.gd
+++ addons/better_base_class/BetterBaseClass.gd
@@ -1,27 +1,27 @@
 class_name BetterBaseClass
 extends RefCounted
-## BetterBaseClass v1.4.0 by swark1n
+## BetterBaseClass v1.5.0 by swark1n
 
 func _init(dict := {}) -> void:
 	for key in dict:
 		if key is not String:
-			print_rich('[color=khaki]Invalid type of ke \'', key, '\'.[/color]')
+			push_warning("Invalid type of key \'", key, "\'.")
 			continue
 
-		@warning_ignore('inference_on_variant')
+		@warning_ignore("inference_on_variant")
 		var other_value := dict.get(key)
 		var other_value_type := typeof(other_value)
 
 		if other_value_type == TYPE_NIL:
-			print_rich('[color=khaki]Received null value from key \'', key, '\'.[/color]')
+			push_warning("Received null value from key \'", key, "\'.")
 			continue
 
 		# Check if property is present in self
 		if not key in self:
-			print_rich('[color=khaki]Unknown key \'', key, '\' with value \'', other_value, '\' of type \'', type_string(other_value_type), '\'.[/color]')
+			push_warning("Unknown key \'", key, "\' with value \'", other_value, "\' of type \'", type_string(other_value_type), "\'.")
 			continue
 
-		@warning_ignore('inference_on_variant')
+		@warning_ignore("inference_on_variant")
 		var self_value := self[key] as Variant
 		var self_value_type := typeof(self_value)
 
@@ -29,18 +29,18 @@ func _init(dict := {}) -> void:
 		if not other_value_type == self_value_type:
 			# Try to convert float to int (JSON decodes int values as float)
 			if other_value_type == TYPE_FLOAT and self_value_type == TYPE_INT:
-				print_rich('[color=khaki]Converting key \'', key, '\' with value \'', other_value, '\' from type \'', type_string(other_value_type), '\' to \'', type_string(self_value_type), '\'.[/color]')
+				push_warning("Converting key \'", key, "\' with value \'", other_value, "\' from type \'", type_string(other_value_type), "\' to \'", type_string(self_value_type), "\'.")
 				other_value = type_convert(other_value, self_value_type)
 			else:
-				print_rich('[color=khaki]Incompatible type of \'', key, '\' with value \'', other_value, '\' type \'', type_string(other_value_type), '\', expected type \'', type_string(self_value_type), '\'.[/color]')
+				push_warning("Incompatible type of \'", key, "\' with value \'", other_value, "\' type \'", type_string(other_value_type), "\', expected type \'", type_string(self_value_type), "\'.")
 				continue
 
 		# Check if array has the correct type
 		if self_value_type == TYPE_ARRAY:
-			@warning_ignore('unsafe_method_access', 'unsafe_cast') var self_arr_type := self_value.get_typed_builtin() as Variant.Type
-			@warning_ignore('unsafe_method_access', 'unsafe_cast') var other_arr_type := other_value.get_typed_builtin() as Variant.Type
+			@warning_ignore("unsafe_method_access", "unsafe_cast") var self_arr_type := self_value.get_typed_builtin() as Variant.Type
+			@warning_ignore("unsafe_method_access", "unsafe_cast") var other_arr_type := other_value.get_typed_builtin() as Variant.Type
 			if not self_arr_type == other_arr_type:
-				print_rich('[color=khaki]Incompatible array type: expected \'', type_string(self_arr_type), '\', got \'', type_string(other_arr_type), '\'.[/color]')
+				push_warning("Incompatible array type: expected \'", type_string(self_arr_type), "\', got \'", type_string(other_arr_type), "\'.")
 				continue
 
 		# Finally:
@@ -126,16 +126,16 @@ static func _take_dict(d: Dictionary, key: String) -> Dictionary:
 
 
 func _to_string() -> String:
-	const SEP := ', '
-	var s := (get_script() as Script).get_global_name()+': '
+	const SEP := ", "
+	var s := (get_script() as Script).get_global_name()+": "
 	var p_list := get_property_list()
 
 	for p in p_list:
-		if p.get('usage') == PROPERTY_USAGE_SCRIPT_VARIABLE:
-			var n := p.get('name')
-			s += n+' = '+str(self[n])+SEP
+		if p.get("usage") == PROPERTY_USAGE_SCRIPT_VARIABLE:
+			var n := p.get("name")
+			s += n+" = "+str(self[n])+SEP
 
-	return '['+s.trim_suffix(SEP)+']'
+	return "["+s.trim_suffix(SEP)+"]"
 
 # non-recursive
 func to_dict() -> Dictionary:
@@ -143,8 +143,8 @@ func to_dict() -> Dictionary:
 	var p_list := get_property_list()
 
 	for p in p_list:
-		if p.get('usage') == PROPERTY_USAGE_SCRIPT_VARIABLE:
-			var n := p.get('name')
+		if p.get("usage") == PROPERTY_USAGE_SCRIPT_VARIABLE:
+			var n := p.get("name")
 			d[n] = self[n]
 
 	return d
